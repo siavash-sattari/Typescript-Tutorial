dbf230f12780b2187966eb210225833b6ed5ed12
siavash sattari <siavash.sattari.75@gmail.com>
2021-11-18 22:32:03 +0330

    A-E01-Compiling TypeScript

---
+++ A/E01/app.js
@@ -0,0 +1,8 @@
+// Compiling TypeScript
+var fName = "John";
+console.log(fName);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ A/E01/app.ts
@@ -0,0 +1,11 @@
+// Compiling TypeScript
+
+var fName = "John";
+console.log(fName);
+
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+
+inputs.forEach((input) => {
+  console.log(input);
+});
