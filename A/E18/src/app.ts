700bf8cbf796c0fbca4d5822d7e1c01ce43b7d52
siavash sattari <siavash.sattari.75@gmail.com>
2021-11-18 23:14:35 +0330

    A-E18-Tuples

---
+++ A/E18/src/app.ts
@@ -0,0 +1,15 @@
+// Tuples
+
+let arr1 = ["john", 30, true];
+arr1[0] = false;
+arr1[1] = "david";
+arr1 = [40, false, "scott"];
+
+let arr2: [string, number, boolean] = ["john", 25, true];
+// arr2[0]=1 ======> Error
+arr2[0] = "mike";
+arr2[1] = 30;
+
+let arr3: [string, number];
+arr3 = ["pizza", 22];
+// arr3 = [22 , "pizza"] ======> Error
